f8188d961196e3904f89e504db32f49d64702e42
Pachev Artem <pachevartem@ya.ru>
2017-12-15 13:12:32 +0300

    SetUp Rider Plugin And Create Timer.cs

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Ez
+{
+
+	/// <summary>
+	/// Описывает работы таймера
+	/// </summary>
+	public class Timer : MonoBehaviour
+	{
+
+		public int StartTimer = 10;  //TODO: сделать считывание с файла 
+
+		/// <summary>
+		/// Запустить отсчет
+		/// </summary>
+		public void BeginTimer()
+		{
+			StartCoroutine(TimeOff(StartTimer));
+		}
+
+		IEnumerator TimeOff(int startTime)   
+		{
+			while (true)
+			{
+				yield return new WaitForSeconds(1);
+				startTime--;
+	
+				if (startTime<0)
+				{
+					Debug.LogError("Вы програли - время кончилось");
+				}
+			}
+		}
+
+	}
+}
